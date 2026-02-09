f181790f500472337a32ab3b7057ebd6e64bcd57
George Danicico <georgedanicico@yahoo.com>
2026-02-09 16:18:53 +0200

    fix - auth fix

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,7 +1,10 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to, from) => {
   const { user } = useAuth()
 
   if (!user.value) {
     return navigateTo('/')
+  }
+  if (user.value && to.path === '/') {
+    return abortNavigation();
   }
 })
